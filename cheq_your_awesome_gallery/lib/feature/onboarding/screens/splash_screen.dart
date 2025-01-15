1fb4eca2d495fcc66831540555d28707bdc4da11
Sifat Ullah Chowdhury <sif.sifat24@gmail.com>
2025-01-16 01:37:49 +0600

    YAG: Update routing on splash screen

--- cheq_your_awesome_gallery/lib/feature/onboarding/screens/splash_screen.dart
+++ cheq_your_awesome_gallery/lib/feature/onboarding/screens/splash_screen.dart
@@ -3,23 +3,30 @@ import 'package:cheq_your_awesome_gallery/resources/asset_helper.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter_svg/flutter_svg.dart';
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  static const path = '/splashScreen';
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(
+      builder: (_) => const SplashScreen(),
+    );
+  }
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
   @override
   void initState() {
     super.initState();
     Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const PermissionScreen()),
+        PermissionScreen.route(),
       );
     });
   }
